3305ff801f25b6cccaa555b697b8f6a65c30fbdc
dimamv20 <148235557+dimamv20@users.noreply.github.com>
2024-09-07 08:27:07 -0600

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,55 +1,47 @@
-const getTotalBalanceByGender = (users, gender) => {
-
-    const filteredUsers = users.filter(user => user.gender === gender);
-
-    let totalBalance = 0;
-    
-
-    for (const user of filteredUsers) {
-        totalBalance += user.balance;
+const getTotalBalanceByGender = (users, gender) => 
+    users
+      .filter(user => user.gender === gender)  
+      .reduce((total, user) => total + user.balance, 0);  
+  
+  const clients = [
+      {
+      name: "Moore Hensley",
+      gender: "male",
+      balance: 2811
+    },
+    {
+      name: "Sharlene Bush",
+      gender: "female",
+      balance: 3821
+    },
+    {
+      name: "Ross Vazquez",
+      gender: "male",
+      balance: 3793
+    },
+    {
+      name: "Elma Head",
+      gender: "female",
+      balance: 2278
+    },
+    {
+      name: "Carey Barr",
+      gender: "male",
+      balance: 3951
+    },
+    {
+      name: "Blackburn Dotson",
+      gender: "male",
+      balance: 1498
+    },
+    {
+      name: "Sheree Anthony",
+      gender: "female",
+      balance: 2764
     }
-    
-
-    return totalBalance;
-};
-
-const clients = [
-    {
-        name: "Moore Hensley",
-        gender: "male",
-        balance: 2811
-    },
-    {
-        name: "Sharlene Bush",
-        gender: "female",
-        balance: 3821
-    },
-    {
-        name: "Ross Vazquez",
-        gender: "male",
-        balance: 3793
-    },
-    {
-        name: "Elma Head",
-        gender: "female",
-        balance: 2278
-    },
-    {
-        name: "Carey Barr",
-        gender: "male",
-        balance: 3951
-    },
-    {
-        name: "Blackburn Dotson",
-        gender: "male",
-        balance: 1498
-    },
-    {
-        name: "Sheree Anthony",
-        gender: "female",
-        balance: 2764
-    }
-];
-
-console.log(getTotalBalanceByGender(clients, "male")); // 12053
-console.log(getTotalBalanceByGender(clients, "female")); // 8863
+  ];
+  
+  console.log(getTotalBalanceByGender(clients, "male")); // 12053
+  
+  console.log(getTotalBalanceByGender(clients, "female")); // 8863
+  
